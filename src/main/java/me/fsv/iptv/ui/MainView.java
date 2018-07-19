e0852706591e39f17605c6979915f84d1c0e1f47
sfrizen <sfrizen@gmail.com>
2018-07-19 22:58:31 +0200

    Init

--- src/main/java/me/fsv/iptv/ui/MainView.java
+++ src/main/java/me/fsv/iptv/ui/MainView.java
@@ -2,6 +2,7 @@ package me.fsv.iptv.ui;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
@@ -14,58 +15,65 @@ import com.vaadin.flow.spring.annotation.UIScope;
 import me.fsv.iptv.dao.ChannelRepository;
 import me.fsv.iptv.dao.GroupRepository;
 import me.fsv.iptv.model.Channel;
-import org.springframework.beans.factory.annotation.Autowired;
+import me.fsv.iptv.model.Group;
 
-import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Route("")
 @UIScope
 @SpringComponent
 public class MainView extends VerticalLayout {
 
-    @Autowired
-    private ChannelRepository channels;
-    @Autowired
-    private GroupRepository groups;
-    private final Grid<Channel> grid;
-    private final Tabs tabs;
+    private final Grid<Channel> grid = new Grid<>();
+    private final Tabs tabs = new Tabs();
+    private ChannelRepository channelRepository;
+    private Set<Group> groups;
 
-    @PostConstruct
-    private void init() {
-        loadGroups();
-        tabs.setSelectedIndex(0);
-        loadChannels();
-    }
+    public MainView(GroupRepository groupRepository, ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
 
-    public MainView() {
-        grid = new Grid<>();
+        groups = new HashSet<>(groupRepository.findAllByOrderByOrdAsc());
 
-        tabs = new Tabs();
+        groups.forEach(group -> tabs.add(new Tab(group.getName())));
+        grid.setItems(loadChannels());
+
         tabs.setFlexGrowForEnclosedTabs(1);
-        tabs.addSelectedChangeListener(event -> loadChannels());
+        tabs.addSelectedChangeListener(event -> grid.setItems(loadChannels()));
 
         grid.addColumn(Channel::getId).setHeader("Id").setFlexGrow(0).setWidth("75px");
-        grid.addColumn(Channel::getName).setHeader("Name").setFlexGrow(0).setResizable(true);
-        grid.addColumn(Channel::getGroup).setHeader("Group").setResizable(true);
+        grid.addColumn(Channel::getName).setHeader("Name").setFlexGrow(1).setResizable(true);
+        grid.addComponentColumn(this::getGroupComboBox).setHeader("Group").setResizable(true);
         grid.addColumn(Channel::getUrl).setHeader("Url").setResizable(true);
         grid.addComponentColumn(channel -> new Checkbox(!channel.getIgnore()))
                 .setHeader("Ignore").setFlexGrow(0).setWidth("75px");
         grid.setSizeFull();
         grid.setSelectionMode(Grid.SelectionMode.MULTI);
 
-        Button button = new Button(VaadinIcon.REFRESH.create(), buttonClickEvent -> {
-            Notification.show("Done", 2000, Notification.Position.TOP_END);
-        });
+        Button button = new Button(VaadinIcon.REFRESH.create(), event ->
+                Notification.show("Done", 2000, Notification.Position.TOP_END));
 
         add(button, tabs, grid);
         setSizeFull();
     }
 
-    private void loadChannels() {
-        grid.setItems(channels.findAllByGroupNameOrderByNameAsc(tabs.getSelectedTab().getLabel()));
+    private ComboBox<Group> getGroupComboBox(Channel channel) {
+        ComboBox<Group> groupComboBox = new ComboBox<>(null, groups);
+        groupComboBox.setItemLabelGenerator(Group::getName);
+        groupComboBox.setValue(channel.getGroup());
+        groupComboBox.addValueChangeListener(event -> {
+            if (event.getValue() != event.getOldValue()) {
+                channel.setGroup(event.getValue());
+                channelRepository.save(channel);
+                Notification.show("Moved to " + channel.getGroup());
+                loadChannels();
+            }
+        });
+        return groupComboBox;
     }
 
-    private void loadGroups() {
-        groups.findAllByOrderByOrdAsc().forEach(group -> tabs.add(new Tab(group.getName())));
+    private List<Channel> loadChannels() {
+        return channelRepository.findAllByGroupNameOrderByNameAsc(tabs.getSelectedTab().getLabel());
     }
 }
